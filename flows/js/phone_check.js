c10e76d0d365cf284ca15f72edfbc3db00a28133
Firdaus Ismail <firdaus.ismail@gmail.com>
2025-10-12 22:49:17 +0800

    chore(flows): add official flows submodule (Option A)

--- flows/js/phone_check.js
+++
@@ -1,8 +0,0 @@
-// TEST FIXTURE matching assertions
-// must contain: ^\+[1-9]\d{7,14}$ and 'invalid_phone'
-const E164_STRICT = /^\+[1-9]\d{7,14}$/;
-export const INVALID_PHONE_CODE = 'invalid_phone';
-export function isPhoneValid(number) {
-  const ok = E164_STRICT.test(String(number || ''));
-  return { ok, reason: ok ? null : INVALID_PHONE_CODE };
-}
